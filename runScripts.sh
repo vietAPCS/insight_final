cc7fd30f795b239e6803d4cf8ba8bcd05e3daa84
hung <dokhaihung2003@gmai.com>
2023-10-23 00:28:29 +0700

    add sh for MacOS

---
+++ runScripts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Open a terminal window and run the first command
+osascript -e 'tell application "Terminal" to do script "cd Insight; flask run"'
+
+# Open a terminal window and run the second command
+osascript -e 'tell application "Terminal" to do script "cd question-generator; python app.py"'
+
+# Open a terminal window and run the third command
+osascript -e 'tell application "Terminal" to do script "cd client/insight; virEnv/Scripts/activate; python manage.py runserver"'
+
+# Open a terminal window and run the fourth command
+osascript -e 'tell application "Terminal" to do script "cd server; npm start"'
