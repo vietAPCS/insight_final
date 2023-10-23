5efe4d76016206ec0175a0ed1c5d19ef19797f00
hung <dokhaihung2003@gmai.com>
2023-10-23 09:46:38 +0700

    add tutorial file

--- runScripts.ps1
+++ runScripts.ps1
@@ -1,4 +1,4 @@
-Start-Process powershell  -ArgumentList  "cd Insight; flask run"
-Start-Process powershell  -ArgumentList  "cd question-generator; python app.py"
-Start-Process powershell  -ArgumentList  "cd client/insight; virEnv/Scripts/activate; python manage.py runserver"
-Start-Process powershell  -ArgumentList  "cd server; npm start" 
+Start-Process powershell -ArgumentList "cd Insight; pip install -r requirements.txt; flask run"
+Start-Process powershell -ArgumentList "cd question-generator; pip install -r requirements.txt; python app.py"
+Start-Process powershell -ArgumentList "cd client/insight; virEnv/Scripts/activate; python manage.py runserver"
+Start-Process powershell -ArgumentList "cd server;npm install; npm start"
--- runScripts.sh
+++ runScripts.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Open a terminal window and run the first command
-osascript -e 'tell application "Terminal" to do script "cd Insight; flask run"'
+osascript -e 'tell application "Terminal" to do script "cd Insight;pip install -r requirements.txt; flask run"'
 
 # Open a terminal window and run the second command
-osascript -e 'tell application "Terminal" to do script "cd question-generator; python app.py"'
+osascript -e 'tell application "Terminal" to do script "cd question-generator;pip install -r requirements.txt; python app.py"'
 
 # Open a terminal window and run the third command
 osascript -e 'tell application "Terminal" to do script "cd client/insight; virEnv/Scripts/activate; python manage.py runserver"'
 
 # Open a terminal window and run the fourth command
-osascript -e 'tell application "Terminal" to do script "cd server; npm start"'
+osascript -e 'tell application "Terminal" to do script "cd server; npm install; npm start"'
